6a45abf2ded9be393aab6dda186063a36c956228
Tsung-Ti (Daniel) Lee <tsungbol90906@gmail.com>
2025-02-14 18:44:41 -0500

    backend milestone

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import dotenv from './db_config/dotenv.js';
+import { Server } from "socket.io";
+import http from "http";
+import app from "./app.js";
+
+const PORT = dotenv.PORT || 5000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+
+    socket.on("getNotification", (data) => {
+        socket.emit("notification", { message: "Fetching notifications..." });
+    })
+});
+
+server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,1 @@
+// Task List / Main Dashboard
